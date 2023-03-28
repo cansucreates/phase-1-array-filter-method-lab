4be523e82d497e360b3e2ba2265cb882ea7ec97f
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-28 15:32:07 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+console.log(findMatching(drivers, "bobby"));
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string));
+}
+
+console.log(fuzzyMatch(drivers,"S"));
+
+function matchName(drivers, string) {
+   return drivers.filter(driver => driver.name === string);
+    
+}
+console.log(matchName(drivers,"Sally"));
